5a466f2b8d680879efb28c748f527a311ae352bc
Colin Park (Hanbin Park) <qkrgksqls9@gmail.com>
2024-10-03 18:09:36 +0900

    Update Main

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -50,12 +50,14 @@ const Home = ({ list: { results }, info }: Props) => {
           style={{ width: "100%", height: "100%", objectFit: "cover" }}
         />
 
-        <Box
+        <Container
+          maxWidth="xl"
           sx={{
             position: "absolute",
             color: "white",
             bottom: 0,
             left: 0,
+            right: 0,
           }}
         >
           <Box
@@ -64,70 +66,71 @@ const Home = ({ list: { results }, info }: Props) => {
               padding: "1rem",
             }}
           >
-            <Typography
-              variant="h2"
-              sx={{
-                fontWeight: "bold",
-              }}
-            >
-              {info.title}
-            </Typography>
-            <Typography
-              sx={{
-                overflow: "hidden",
-                textOverflow: "ellipsis",
-                display: "-webkit-box",
-                WebkitLineClamp: "4",
-                WebkitBoxOrient: "vertical",
-              }}
-            >
-              {info.overview}
-            </Typography>
-          </Box>
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "end",
-              alignItems: "center",
-              color: "white",
-              "& > *": {
-                marginBottom: 2,
-              },
-              px: 2,
-            }}
-          >
-            <ButtonGroup
-              sx={{
-                justifyContent: "end",
-                textDecoration: "none",
-                color: "white",
-              }}
-            >
-              <Button
-                disabled
-                key="rating"
+            <Box>
+              <Typography
+                variant="h2"
                 sx={{
-                  ":disabled": {
-                    color: "inherit",
-                    border: "none",
-                  },
+                  fontWeight: "bold",
                 }}
               >
-                평점: {Math.floor(info.vote_average * 10)}/100
-              </Button>
-              <Button
-                key="detail"
+                {info.title}
+              </Typography>
+              <Typography
                 sx={{
+                  overflow: "hidden",
+                  textOverflow: "ellipsis",
+                  display: "-webkit-box",
+                  WebkitLineClamp: "4",
+                  WebkitBoxOrient: "vertical",
+                }}
+              >
+                {info.overview}
+              </Typography>
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "end",
+                alignItems: "center",
+                color: "white",
+                px: 2,
+                py: 1,
+              }}
+            >
+              <Box
+                sx={{
+                  justifyContent: "end",
+                  textDecoration: "none",
                   color: "white",
                 }}
-                href={`/movie/details/${info.id}`}
               >
-                <Info sx={{ pr: 1 }} />
-                상세보기
-              </Button>
-            </ButtonGroup>
+                <Button
+                  disabled
+                  key="rating"
+                  sx={{
+                    ":disabled": {
+                      color: "inherit",
+                      border: "none",
+                    },
+                  }}
+                >
+                  평점: {Math.floor(info.vote_average * 10)}/100
+                </Button>
+                <Button
+                  key="detail"
+                  sx={{
+                    color: "white",
+                    bgcolor: "rgba(255,255,255,0.2)",
+                  }}
+                  href={`/movie/details/${info.id}`}
+                >
+                  <Info sx={{ pr: 1 }} />
+                  상세보기
+                </Button>
+              </Box>
+            </Box>
           </Box>
-        </Box>
+        </Container>
       </Box>
 
       <CardList list={results} />
