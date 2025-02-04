aa56b71e0c7bdf38333724aed6dd0adca8d67a2f
Colin Park (Hanbin Park) <qkrgksqls9@gmail.com>
2025-02-04 15:56:28 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,145 +7,137 @@ import { Box, Button } from "@mui/material";
 import { Info } from "@mui/icons-material";
 import CarouselSection from "@/components/organisms/CarouselSection";
 
-// const getList = async () => {
-//   const { results: now_playing } = await getMovieList("now_playing");
-//   const { results: popular } = await getMovieList("popular");
-//   const { results: top_rated } = await getMovieList("top_rated");
-//   const { results: upcoming } = await getMovieList("upcoming");
+const getList = async () => {
+  const { results: now_playing } = await getMovieList("now_playing");
+  // const { results: popular } = await getMovieList("popular");
+  // const { results: top_rated } = await getMovieList("top_rated");
+  // const { results: upcoming } = await getMovieList("upcoming");
 
-//   const main = await getMovieInfo(now_playing[0].id);
+  const main = await getMovieInfo(now_playing[0].id);
 
-//   return {
-//     now_playing,
-//     popular,
-//     top_rated,
-//     upcoming,
-//     main,
-//   };
-// };
-
-// export default async function Home() {
-//   const { now_playing, popular, top_rated, upcoming, main } = await getList();
-//   const mainImgURL = convertImageURL(main.backdrop_path, "original");
-
-//   return (
-//     <>
-//       <Box sx={{ position: "relative", height: "50rem" }}>
-//         <Box
-//           component={"img"}
-//           sx={{
-//             width: "100%",
-//             height: "100%",
-//             objectFit: "cover",
-//             display: "block",
-//           }}
-//           loading="lazy"
-//           src={mainImgURL}
-//           alt={main.title}
-//         />
-
-//         <Box
-//           sx={{
-//             color: "white",
-//             position: "absolute",
-//             bottom: 0,
-//             left: 0,
-//             right: 0,
-//             backgroundImage:
-//               "linear-gradient(0deg, rgba(0,0,0,1), rgba(255,255,255, 0))",
-//           }}
-//         >
-//           <Container maxWidth="xl" sx={{}}>
-//             <Box
-//               sx={{
-//                 maxWidth: "500px",
-//               }}
-//             >
-//               <Box>
-//                 <Typography
-//                   variant="h2"
-//                   sx={{
-//                     fontWeight: "bold",
-//                   }}
-//                 >
-//                   {main.title}
-//                 </Typography>
-//                 <Typography
-//                   sx={{
-//                     overflow: "hidden",
-//                     textOverflow: "ellipsis",
-//                     display: "-webkit-box",
-//                     WebkitLineClamp: "4",
-//                     WebkitBoxOrient: "vertical",
-//                   }}
-//                 >
-//                   {main.overview}
-//                 </Typography>
-//               </Box>
-//               <Box
-//                 sx={{
-//                   display: "flex",
-//                   justifyContent: "end",
-//                   alignItems: "center",
-//                   color: "white",
-//                   py: 2,
-//                 }}
-//               >
-//                 <Box
-//                   sx={{
-//                     justifyContent: "end",
-//                     textDecoration: "none",
-//                     color: "white",
-//                   }}
-//                 >
-//                   <Button
-//                     disabled
-//                     key="rating"
-//                     sx={{
-//                       ":disabled": {
-//                         color: "inherit",
-//                         border: "none",
-//                       },
-//                     }}
-//                   >
-//                     평점: {Math.floor(main.vote_average * 10)}/100
-//                   </Button>
-//                   <Button
-//                     key="detail"
-//                     sx={{
-//                       color: "white",
-//                       bgcolor: "rgba(255,255,255,0.2)",
-//                     }}
-//                     href={`/movie/details/${main.id}`}
-//                   >
-//                     <Info sx={{ pr: 1 }} />
-//                     상세보기
-//                   </Button>
-//                 </Box>
-//               </Box>
-//             </Box>
-//           </Container>
-//         </Box>
-//       </Box>
-//       <Container maxWidth="xl">
-//         {/* 상영 중인 영화 */}
-//         <CarouselSection title="상영 중인 영화" list={now_playing} />
-
-//         {/* 인기 상영작 */}
-//         <CarouselSection title="인기 상영작" list={popular} />
-
-//         {/* 최고 평점 영화 */}
-//         <CarouselSection title="최고 평점 영화" list={top_rated} />
-
-//         {/* 최근 개봉, 예정작 */}
-//         <CarouselSection title="최근 개봉, 예정작" list={upcoming} />
-//       </Container>
-//     </>
-//   );
-// }
-
-const Home = () => {
-  return <div>hello</div>;
+  return {
+    // now_playing,
+    // popular,
+    // top_rated,
+    // upcoming,
+    main,
+  };
 };
 
-export default Home;
+export default async function Home() {
+  const { main } = await getList();
+  const mainImgURL = convertImageURL(main.backdrop_path, "original");
+
+  return (
+    <>
+      <Box sx={{ position: "relative", height: "50rem" }}>
+        <Box
+          component={"img"}
+          sx={{
+            width: "100%",
+            height: "100%",
+            objectFit: "cover",
+            display: "block",
+          }}
+          loading="lazy"
+          src={mainImgURL}
+          alt={main.title}
+        />
+
+        <Box
+          sx={{
+            color: "white",
+            position: "absolute",
+            bottom: 0,
+            left: 0,
+            right: 0,
+            backgroundImage:
+              "linear-gradient(0deg, rgba(0,0,0,1), rgba(255,255,255, 0))",
+          }}
+        >
+          <Container maxWidth="xl" sx={{}}>
+            <Box
+              sx={{
+                maxWidth: "500px",
+              }}
+            >
+              <Box>
+                <Typography
+                  variant="h2"
+                  sx={{
+                    fontWeight: "bold",
+                  }}
+                >
+                  {main.title}
+                </Typography>
+                <Typography
+                  sx={{
+                    overflow: "hidden",
+                    textOverflow: "ellipsis",
+                    display: "-webkit-box",
+                    WebkitLineClamp: "4",
+                    WebkitBoxOrient: "vertical",
+                  }}
+                >
+                  {main.overview}
+                </Typography>
+              </Box>
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "end",
+                  alignItems: "center",
+                  color: "white",
+                  py: 2,
+                }}
+              >
+                <Box
+                  sx={{
+                    justifyContent: "end",
+                    textDecoration: "none",
+                    color: "white",
+                  }}
+                >
+                  <Button
+                    disabled
+                    key="rating"
+                    sx={{
+                      ":disabled": {
+                        color: "inherit",
+                        border: "none",
+                      },
+                    }}
+                  >
+                    평점: {Math.floor(main.vote_average * 10)}/100
+                  </Button>
+                  <Button
+                    key="detail"
+                    sx={{
+                      color: "white",
+                      bgcolor: "rgba(255,255,255,0.2)",
+                    }}
+                    href={`/movie/details/${main.id}`}
+                  >
+                    <Info sx={{ pr: 1 }} />
+                    상세보기
+                  </Button>
+                </Box>
+              </Box>
+            </Box>
+          </Container>
+        </Box>
+      </Box>
+      {/* 
+      <Container maxWidth="xl">
+        <CarouselSection title="상영 중인 영화" list={now_playing} />
+
+        <CarouselSection title="인기 상영작" list={popular} />
+
+        <CarouselSection title="최고 평점 영화" list={top_rated} />
+
+        <CarouselSection title="최근 개봉, 예정작" list={upcoming} />
+      </Container>
+       */}
+    </>
+  );
+}
